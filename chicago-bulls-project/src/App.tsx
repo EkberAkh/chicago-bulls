fb886fdf1cab60a45edbe04ccd072490c480fbfe
EkberAkh <ekber.0303@gmail.com>
2023-10-11 20:03:25 +0400

    add structure

--- chicago-bulls-project/src/App.tsx
+++ chicago-bulls-project/src/App.tsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import logo from "./assets/logo.png";
+import cart from "./assets/shopping-cart.png";
+import { Select, Space } from "antd";
+import { Route, Routes, Link } from "react-router-dom";
+import Team from "./pages/Team";
+import Home from "./pages/Home";
+import Gallery from "./pages/Gallery";
+import News from "./pages/News";
+import Shop from "./pages/Shop";
 
+const handleChange = (value: string) => {
+  console.log(`selected ${value}`);
+};
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React + Typescript</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <div className="container">
+          <div className="header-items">
+            <div className="header-logo">
+              <img src={logo} alt="logo" />
+            </div>
+            <nav>
+              <ul>
+                <li >
+                  <Link className="link active" to="/">Home</Link>
+                </li>
+                <li>
+                  <Link className="link" to="/team">Team</Link>
+                </li>
+                <li>
+                  <Link className="link" to="/gallery">Gallery</Link>
+                </li>
+                <li>
+                  <Link className="link" to="/news">News</Link>
+                </li>
+
+                <li>
+                  <Link className="link" to="/shop">Shop</Link>
+                </li>
+              </ul>
+            </nav>
+            <div className="header-end">
+              <img src={cart} alt="" />
+              <Space wrap>
+    <Select
+      defaultValue="en"
+      style={{ width: 120 }}
+      onChange={handleChange}
+      options={[
+        { value: 'az', label: 'AZ' },
+        { value: 'en', label: 'EN' },
+        { value: 'ru', label: 'RU' },
+      ]}
+    />
+ 
+  </Space>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
