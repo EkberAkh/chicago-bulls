5afdef6719949c5683d754ce4cadc1c7da43c937
EkberAkh <ekber.0303@gmail.com>
2023-11-26 14:29:32 +0400

    add alert updates

--- client/src/components/Header/SweetAlert/Buy.tsx
+++ client/src/components/Header/SweetAlert/Buy.tsx
@@ -1,6 +1,6 @@
-import "./Buy.css";
-import Swal from "sweetalert2";
-import withReactContent from "sweetalert2-react-content";
+import "./Buy.css"
+import Swal from "sweetalert2"
+import withReactContent from 'sweetalert2-react-content';
 import { useCart } from "../../Context";
 import { useTranslation } from "react-i18next";
 
@@ -8,51 +8,47 @@ const MySwal = withReactContent(Swal);
 
 export const Buy = () => {
   const { t } = useTranslation();
-  const context = useCart();
-  const swalWithBootstrapButtons = MySwal.mixin({
-    customClass: {
-      confirmButton: "btn btn-success",
-      cancelButton: "btn btn-danger",
-    },
-    //   buttonsStyling: false,
-  });
+    const context = useCart();
+    const swalWithBootstrapButtons = MySwal.mixin({
+        customClass: {
+            confirmButton: "btn btn-success",
+            cancelButton: "btn btn-danger"
+          },
+        //   buttonsStyling: false,
+    });
 
-  const sweetHandler = () => {
-    swalWithBootstrapButtons
-      .fire({
-        title: t("AreYouSure"),
-        text: t("YouConfirm"),
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonText: t("OfCourse"),
-        cancelButtonText: t("Cancel"),
-        reverseButtons: true,
-      })
-      .then((result) => {
-        if (result.isConfirmed) {
-          swalWithBootstrapButtons.fire({
-            title: t("Completed"),
-            text: t("Successfully"),
-            icon: "success",
+    const sweetHandler = () => {
+        swalWithBootstrapButtons.fire({
+            title: t("AreYouSure"),
+            text: t("YouConfirm"),
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: t("OfCourse"),
+            cancelButtonText: t("Cancel"),
+            reverseButtons: true
+          }).then((result) => {
+            if (result.isConfirmed) {
+              swalWithBootstrapButtons.fire({
+                title: t("Completed"),
+                text: t("Successfully"),
+                icon: "success"
+              });
+              context?.removeAllProducts();
+            } else if (
+              /* Read more about handling dismissals below */
+              result.dismiss === Swal.DismissReason.cancel
+            ) {
+              swalWithBootstrapButtons.fire({
+                title: t("Cancelled"),
+                text: t("NotCompleted"),
+                icon: "error"
+              });
+            }
           });
-          context?.removeAllProducts();
-        } else if (
-          /* Read more about handling dismissals below */
-          result.dismiss === Swal.DismissReason.cancel
-        ) {
-          swalWithBootstrapButtons.fire({
-            title: t("Cancelled"),
-            text: t("NotCompleted"),
-            icon: "error",
-          });
-        }
-      });
-  };
+    }
   return (
     <div>
-      <button className="button" onClick={sweetHandler}>
-        {t("BuyProducts")}
-      </button>
+      <button className="button" onClick={sweetHandler}>{t("BuyProducts")}</button>
     </div>
-  );
-};
+  )
+}
